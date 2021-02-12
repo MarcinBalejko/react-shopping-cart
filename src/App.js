7d037762847c4332a69486b9169156368d0bd997
MarcinBalejko <mbbm9@o2.pl>
2021-02-13 00:44:31 +0100

    :art::sparkles: Added SASS, removed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "./sass/main.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Cart from "./components/Cart";
